496f7da36fc1832703a2b7bde26213cada75942a
GitHack27 <arceusxy2003@gmail.com>
2025-04-05 21:32:47 +0530

    Initial Commit

---
+++ src/app/Shared/one-line-banner/one-line-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-line-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './one-line-banner.component.html',
+  styleUrl: './one-line-banner.component.css'
+})
+export class OneLineBannerComponent {
+
+}
